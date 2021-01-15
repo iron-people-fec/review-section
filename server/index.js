643ab15c62ee8de1c1b78ed60b7b9ab21162b6ea
Lexy Kramer <lexy.kramer@gmail.com>
2021-01-15 00:45:50 -0500

    Established database connection using sequelize

--- server/index.js
+++ server/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const db = require('../database')
+const Review = require('../database/models.js').Review
+const Photo = require('../database/models.js').Photo
 
+// try {
+//   db.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
@@ -11,6 +20,8 @@ app.get('/', (req, res) => {
   res.render()
 })
 
-app.listen(port, () => {
-  console.log(`Now listening on port ${port}`)
+db.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Now listening on port ${port}`)
+  })
 })
