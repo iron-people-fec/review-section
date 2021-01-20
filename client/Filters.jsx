0ab3ac06083240fcd33a2b98d940f228834181ec
Lexy Kramer <lexy.kramer@gmail.com>
2021-01-20 02:15:34 -0500

    Added dropdown buttons - no functionality yet

--- client/Filters.jsx
+++ client/Filters.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Filters(props) {
   return (
-    <div>
-
-    </div>
+    <Container>
+      <Dropdown>sort by <strong>most recent</strong></Dropdown>
+      <Dropdown>filter by <strong>all ratings</strong></Dropdown>
+    </Container>
   )
 }
+
+const Container = styled.div`
+display: flex;
+align-items: center;
+background-color: #F7F7F7;
+margin: 20px 0;
+padding: 1.5em 17px;
+`
+
+const Dropdown = styled.span`
+display: inline-block;
+padding: 10px 20px;
+border:#888888 solid 1px;
+  border-radius: 4px;
+  background-color: white;
+  color: #333333;
+  margin-right: 1em;
+`
 
 export default Filters;
