3a8f6ac7d149acb6d5fd69ad8fadec490c6e9742
Lexy Kramer <lexy.kramer@gmail.com>
2021-01-30 01:20:32 -0500

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('bullseye_reviews', 'root', '', {
+module.exports = new Sequelize('bullseye_reviews', 'root', 'Shinbobo88!', {
   host: 'localhost',
   dialect: 'mysql'
 });
